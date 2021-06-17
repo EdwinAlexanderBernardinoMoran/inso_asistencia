b3e13c541c839ff0e52217b812559cc4042fd596
AnaMoran45 <tulamorananadoris@gmail.com>
2021-06-17 14:30:08 -0600

    modelo hecho

---
+++ default/app/models/matriculas.php
@@ -0,0 +1,9 @@
+<?php
+
+    class Matriculas extends ActiveRecord{
+        public function getMatriculas($page, $ppage=20){
+            return $this->paginate("page: $page", "per_page: $ppage", 'order: id desc');
+        }
+    }
+
+?>
