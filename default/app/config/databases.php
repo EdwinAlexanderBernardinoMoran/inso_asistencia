35e118da6b966bc98eb933796b9199cdfb3e3a88
Juan Navarro <navnelson7@gmail.com>
2021-06-21 13:35:40 -0600

    arreglar enlaces de navbar

--- default/app/config/databases.php
+++ default/app/config/databases.php
@@ -12,11 +12,11 @@ return [
         /**
          * username: usuario con permisos en la base de datos
          */
-        'username' => 'root', //no es recomendable usar el usuario root
+        'username' => 'nelson', //no es recomendable usar el usuario root
         /**
          * password: clave del usuario de la base de datos
          */
-        'password' => '',
+        'password' => 'lasttip2020',
         /**
          * test: nombre de la base de datos
          */
--- default/app/controllers/especialidades_controller.php
+++ default/app/controllers/especialidades_controller.php
@@ -2,8 +2,11 @@
     Load::models('especialidades');
 
     class EspecialidadesController extends AppController{
-        public function index(){
-            
+        public function index($page=1){
+            View::template('principal');
+            $especialidad = new Especialidades();
+            $this->especialidades = $especialidad->getEspecialidades('$page');
+
         }
     }
 ?>
