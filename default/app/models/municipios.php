470b4b650a2230c535746ba774d2d7fab5c62096
RocioLue <2017391@clases.edu.sv>
2021-07-01 10:55:39 -0600

    creando controller y vista de Municipios

--- default/app/models/municipios.php
+++ default/app/models/municipios.php
@@ -1,9 +1,10 @@
-<?php 
+<?php
 
 class Municipios extends ActiveRecord{
     public function getMunicipios($page, $ppage=20){
-        return $this->paginate("page: $page", "per_page: $ppage", 'order: id_municipio desc');
+        return $this->paginate("page: $page", "per_page: $ppage", 'order: id_minicipio desc'); 
     }
 }
 
 ?>
+
