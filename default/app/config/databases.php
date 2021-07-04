0eb63b3e7ffa838c6d928bd493f44e79e1df7d7a
TatianaDePaz <lisbethtati305@gmail.com>
2021-07-04 16:54:15 -0600

    arreglos de zona_controller

--- default/app/config/databases.php
+++ default/app/config/databases.php
@@ -20,7 +20,7 @@ return [
         /**
          * test: nombre de la base de datos
          */
-        'name'     => 'asistencias_inso',
+        'name'     => 'asistencias',
         /**
          * type: tipo de motor de base de datos (mysql, pgsql, oracle o sqlite)
          */
--- default/app/controllers/zonas_controller.php
+++ default/app/controllers/zonas_controller.php
@@ -16,12 +16,14 @@ class ZonasController extends AppController
             $this->titulo="zonas";
             if (Input::hasPost('zonas')){
                 $zona = new Zonas(Input::post('zonas'));
-            if (!$zona->create()){
-            Flash::valid("zona creada exitosamente");
-            Input::delete();
+            if (!$zona->create(Input::post('zonas'))){
+            Flash::error('fallo la creacion de zona');
+            Flash::valid('zona creada exitosamente');
+            }else{
+           
+           Input::delete();
             return Redirect::to();
             }
-           Flash::error("fallo la operacion");
         }
     }
     /**
@@ -33,13 +35,13 @@ class ZonasController extends AppController
         $zona = new Zonas();
         if (Input::hasPost('zonas')){
             if (!$zona->update(Input::post('zonas'))){
-                Flash::error("fallo la operacion");
-            }else{
-                Flash::valid("creado exitosamente");
+                Flash::error("fallo la edicion de la zona");
+            } else {
+                Flash::valid("creada exitosamente la zona");
                 return Redirect::to();
             }
-        }else{
-            $this->zonas = $zona->Find((int)$id); 
+        } else {
+            $this->zonas = $zona->find((int)$id); 
         }
     }
     /**
@@ -47,7 +49,7 @@ class ZonasController extends AppController
      */
     public function del($id){
         $zona = new Zonas();
-        if(!$zona->delate((int)$id)){
+        if(!$zona->delete((int)$id)){
             Flash::error("error al ingresar la zona");
         }else{
             Flash::valid("ingreso con exito");
