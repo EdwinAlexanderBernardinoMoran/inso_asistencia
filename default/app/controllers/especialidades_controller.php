789e676d1a8d9a73beabb528d2ddb0e4a5216a1c
Juan Navarro <navnelson7@gmail.com>
2021-07-09 16:19:06 -0600

    Guardado de Especialidades funcionando

--- default/app/controllers/especialidades_controller.php
+++ default/app/controllers/especialidades_controller.php
@@ -12,6 +12,16 @@ class EspecialidadesController extends AppController{
     public function create(){
         View::template('principal');
         $this->titulo = "Creando Especialidad";
+
+        if(Input::hasPost('especialidades')){
+            $especialidad = new Especialidades(Input::post('especialidades'));
+            if(!$especialidad->save()){
+                Flash::error('Fallo la operacion');
+            }else{
+                Flash::valid('Operacion exitosa');
+                Input::delete();
+            }
+        }
     }
 }
 
