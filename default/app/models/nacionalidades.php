ede1c752f44efabcde0d6009cac7d9f5817a8a37
Edwin Alexander <814892@clases.edu.sv>
2021-11-04 16:02:11 -0600

    cambiando una funcion del contralador

--- default/app/models/nacionalidades.php
+++ default/app/models/nacionalidades.php
@@ -2,7 +2,7 @@
 
 class Nacionalidades extends ActiveRecord{
     public function getNacionalidades($page, $ppage=20){
-        // $naciona = Load::model('nacionalidades')->find_by_sql("select nacionalidades.nombre_nacionalidad from nacionalidades where nacionalidades.id=alumnos.id_nacionalidad");
+
         return $this->paginate("page: $page", "per_page: $ppage", 'order: id desc');
     }
 }
