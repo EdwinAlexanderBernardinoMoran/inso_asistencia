0d28b1e61741f6d7f258660319987c9047eec8da
AnaMoran45 <tulamorananadoris@gmail.com>
2021-11-09 09:34:48 -0600

    Buscador funcional

--- default/app/models/matriculas.php
+++ default/app/models/matriculas.php
@@ -5,11 +5,7 @@
             return $this->paginate("page: $page", "per_page: $ppage", 'order: id desc');
         }
 
-        public function buscar($criterio){
-            $sql= "SELECT * FROM `matriculas` WHERE id_alumnos LIKE '%{$criterio}%'";
-           $matriculas1 = (new Matriculas)->find_all_by_sql($sql);
-           return $matriculas1;
-        }
+       
     }
 
 ?>
