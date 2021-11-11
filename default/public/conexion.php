fe0d0a45672be466cc54b50e873f9beed9223ee1
navnelson7 <navnelson7@gmail.com>
2021-11-11 15:38:29 -0600

    actualizando gitignore

--- default/public/conexion.php
+++ default/public/conexion.php
@@ -4,7 +4,7 @@
     {
         $host='localhost';
         $user='root';
-        $pass='';
+        $pass='lasttip2021';
         $db='asistencias';
 
         $conexion=mysqli_connect($host,$user,$pass,$db);
